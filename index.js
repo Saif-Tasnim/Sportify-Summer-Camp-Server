ce1294d1a402bb3a57b6ca814ab381d4ea7463bd
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-16 17:34:19 +0600

    json web token set

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -30,12 +31,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("Sportify-Summer-Camp").collection("user");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // user info
+    app.post('/users',async (req,res) => {
+        const user = req.body;
+        const query = {email : user.email}
+        const found = await userCollection.findOne(query)
+        if(found){
+            return res.send("user already exist");
+        }
+
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
