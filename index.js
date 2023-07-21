4557b2ab9894591b93d953c548622cc9f9e2100d
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-21 09:46:08 +0600

    verify jwt done

--- index.js
+++ index.js
@@ -11,9 +11,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/' , (req,res) => {
-    res.send("Sportify server is opened")
+app.get('/', (req, res) => {
+  res.send("Sportify server is opened")
 })
+
+// verify JWT
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+
+  // it will carry bearer token thats why it has to split 
+  const token = authorization.split(" ")[1]
+
+  const check = token === process.env.JSON_SECRET_KEY;
+  
+  jwt.verify(token, process.env.JSON_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+
+    req.decoded = decoded;
+    next();
+  })
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.ectfhk2.mongodb.net/?retryWrites=true&w=majority`;
@@ -38,16 +62,29 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // user info
-    app.post('/users',async (req,res) => {
-        const user = req.body;
-        const query = {email : user.email}
-        const found = await userCollection.findOne(query)
-        if(found){
-            return res.send("user already exist");
-        }
+    app.get('/users' , verifyJWT , async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
 
-        const result = await userCollection.insertOne(user);
-        res.send(result);
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const found = await userCollection.findOne(query)
+      if (found) {
+        return res.send("user already exist");
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JSON_SECRET_KEY, { expiresIn: '1h' })
+      res.send({ token })
 
     })
 
@@ -60,6 +97,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port , ()=>{
-    console.log(`${port} is set for sportify server`);
+app.listen(port, () => {
+  console.log(`${port} is set for sportify server`);
 })
